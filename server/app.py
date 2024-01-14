d41464856a98291c96be88669bc0b5bd50c89f54
byron korir <byron.korir@student.moringaschool.com>
2024-01-14 14:26:55 +0300

    complete backend bt client is outdated

--- server/app.py
+++ server/app.py
@@ -7,7 +7,7 @@ from models import db, Article, User
 
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
@@ -23,12 +23,21 @@ def clear_session():
 @app.route('/articles')
 def index_articles():
 
-    pass
+    articles = [article.to_dict() for article in Article.query.all()]
+    return make_response(jsonify(articles),200)
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    session['page_views'] = session.get('page_views') or 0
+    session['page_views'] += 1
 
-    pass
+    if session['page_views'] <=3:
+        article = Article.query.get(id).to_dict()
+        return make_response(jsonify(article),200)
+
+    return {"message":'Maximum pageview limit reached'},400
+
+
 
 if __name__ == '__main__':
     app.run(port=5555)
